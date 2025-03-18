b0b0269570c5d904f1f2bf27200f509f074c39b0
PPtimynti <puputimayanti006@gmail.com>
2025-03-18 09:23:10 +0700

    Commit terupdate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta name="csrf-token" content="{{ csrf_token() }}">
    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+   <link rel="icon" type="image/png" href="{{ asset('images/pln.png') }}" style="width: 32px;">
 
    <title>{{ config('app.name', 'Laravel') }}</title>
 
